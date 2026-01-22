bf13f0b56e1ea560d54e01adc1e49aaf313bb910
Pranjl7 <pranjalsgbhatt@gmail.com>
2026-01-22 17:02:59 +0530

    feat: add user sync api in app layout to authenticate and create user

--- web/src/App/layout/AppLayout.jsx
+++ web/src/App/layout/AppLayout.jsx
@@ -1,8 +1,26 @@
 import React from 'react';
 import AppNavbar from '../components/AppNavbar';
 import AppRoutes from '../routes/AppRoutes';
+import { useUser, useAuth } from '@clerk/clerk-react';
+import { useEffect } from 'react';
+import axios from 'axios'
 
 function AppLayout() {
+  const { isLoaded, isSignedIn } = useUser();
+  const { getToken } = useAuth();
+  useEffect(() => {
+    if (isLoaded && isSignedIn) {
+      usersignedin();
+    }
+  }, [isLoaded, isSignedIn]);
+
+  async function usersignedin() {
+    const token = await getToken();
+
+    await axios.post('http://localhost:5000/api/v1/user/sync',{}, {
+      headers: { Authorization: `Bearer ${token}` },
+    });
+  }
   return (
     <div className='font-baloo relative min-h-screen max-w-screen'>
       <AppNavbar />
