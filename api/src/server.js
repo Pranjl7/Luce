447bd1d52e7801eec06dd87fc56eb0fbd31cb5d9
Pranjl7 <pranjalsgbhatt@gmail.com>
2026-01-25 14:55:16 +0530

    feat: add express.urlencoded formdata parse middleware

--- api/src/server.js
+++ api/src/server.js
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { urlencoded } from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 dotenv.config();
@@ -10,17 +10,27 @@ const port = process.env.PORT || 5000;
 const app = express();
 
 // cors
-app.use(cors({
-  origin: process.env.FRONTEND_URL,
-  allowedHeaders: ["Content-Type", "Authorization"],
-  credentials: true
-}))
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  }),
+);
 
 // db
 import connectdb from './config/mongodb.config.js';
 
 // middlewares
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.json({
+    success: true,
+    message: 'Hello Luce!',
+  });
+});
 
 // routes
 app.use('/api/v1/home', publicRoutes);
