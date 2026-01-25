9985a21dae04703acdc7c18b4eb8f2a99ad93143
Pranjl7 <pranjalsgbhatt@gmail.com>
2026-01-26 00:20:30 +0530

    feat: add props for dynamic data loading/ rendering.

--- web/src/App/ui/Homepage-Reactions.jsx
+++ web/src/App/ui/Homepage-Reactions.jsx
@@ -1,12 +1,12 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Reactions from './Reactions';
 
-function HomePageReactions() {
+function HomePageReactions({ likes, comments, createdat }) {
   return (
     <div className='flex items-center justify-between'>
-      <Reactions />
+      <Reactions likes={likes} comments={comments} />
       <div className='flex items-center gap-x-4'>
-        <p className='text-sm text-black/70'>12 Apr, 2006</p>
+        <p className='text-sm text-black/70'>{createdat}</p>
         <img className='size-6' src='/assets/bookmark-light.svg' alt='image' />
       </div>
     </div>
--- web/src/App/ui/Reactions.jsx
+++ web/src/App/ui/Reactions.jsx
@@ -1,18 +1,18 @@
-import React from 'react'
+import React from 'react';
 
-function Reactions() {
+function Reactions({ likes, comments }) {
   return (
     <div className='flex items-center gap-x-5'>
-        <div className='flex items-center gap-x-1 cursor-pointer'>
-          <img className='size-4' src='/assets/heart-dark.svg' alt='image' />
-          <p className='text-sm text-black/70'>10</p>
-        </div>
-        <div className='flex items-center gap-x-1 cursor-pointer'>
-          <img className='size-4' src='/assets/comment-dark.svg' alt='image' />
-          <p className='text-sm text-black/70'>10</p>
-        </div>
+      <div className='flex cursor-pointer items-center gap-x-1'>
+        <img className='size-4' src='/assets/heart-dark.svg' alt='image' />
+        <p className='text-sm text-black/70'>{likes}</p>
       </div>
-  )
+      <div className='flex cursor-pointer items-center gap-x-1'>
+        <img className='size-4' src='/assets/comment-dark.svg' alt='image' />
+        <p className='text-sm text-black/70'>{comments.length}</p>
+      </div>
+    </div>
+  );
 }
 
-export default Reactions
+export default Reactions;
