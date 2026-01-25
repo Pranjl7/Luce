7fd534e2003092321db195b1179e0f03e65ed146
Pranjl7 <pranjalsgbhatt@gmail.com>
2026-01-26 00:17:38 +0530

    feat: add import meta method for safe accessing Vite-url

--- web/src/App/layout/AppLayout.jsx
+++ web/src/App/layout/AppLayout.jsx
@@ -3,11 +3,12 @@ import AppNavbar from '../components/AppNavbar';
 import AppRoutes from '../routes/AppRoutes';
 import { useUser, useAuth } from '@clerk/clerk-react';
 import { useEffect } from 'react';
-import axios from 'axios'
+import axios from 'axios';
 
 function AppLayout() {
   const { isLoaded, isSignedIn } = useUser();
   const { getToken } = useAuth();
+  
   useEffect(() => {
     if (isLoaded && isSignedIn) {
       usersignedin();
@@ -17,9 +18,13 @@ function AppLayout() {
   async function usersignedin() {
     const token = await getToken();
 
-    await axios.post('http://localhost:5000/api/v1/user/sync',{}, {
-      headers: { Authorization: `Bearer ${token}` },
-    });
+    await axios.post(
+      `${import.meta.env.VITE_API_URL}/api/v1/user/sync`,
+      {},
+      {
+        headers: { Authorization: `Bearer ${token}` },
+      }
+    );
   }
   return (
     <div className='font-baloo relative min-h-screen max-w-screen'>
