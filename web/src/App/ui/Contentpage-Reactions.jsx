41fa8e15e7bb3bccf80c7cb0566b5ae36304134a
Pranjl7 <pranjalsgbhatt@gmail.com>
2026-01-29 16:24:15 +0530

    revert: replace testing static data with dynamic data loading through props

--- web/src/App/ui/Contentpage-Reactions.jsx
+++ web/src/App/ui/Contentpage-Reactions.jsx
@@ -1,23 +1,37 @@
 import React from 'react';
 import Reactions from './Reactions';
 
-function ContentPageReactions() {
+function ContentPageReactions({user, content}) {
   return (
     <div className='flex w-full justify-between border-y border-black/10 py-3'>
       <div className='flex items-center gap-x-5'>
         <p className='text-md font-medium text-black/70'>
           <img
             className='mr-2 inline-block size-8 rounded-full'
-            src='https://img.clerk.com/eyJ0eXBlIjoicHJveHkiLCJzcmMiOiJodHRwczovL2ltYWdlcy5jbGVyay5kZXYvb2F1dGhfZ29vZ2xlL2ltZ18zOElOdExZY1NmS3VzWGNNWVdtQ1VjZmtNMEIifQ?width=160'
-            alt=''
+            src={user.avatarurl}
+            alt='image'
           />
-          Pranjal Bhatt
+          {user.username}
+          <img
+              src='/assets/verified.svg'
+              alt='image'
+              className='ml-1 inline-block size-4'
+            />
         </p>
         <button className='text-md fond-medium cursor-pointer rounded-full border border-black/80 px-4 py-1'>
           Follow
         </button>
       </div>
-      <Reactions />
+      <div className='flex items-center gap-x-5'>
+      <div className='flex cursor-pointer items-center gap-x-1'>
+        <img className='size-4' src='/assets/heart-dark.svg' alt='image' />
+        <p className='text-sm text-black/70'>{content.likes}</p>
+      </div>
+      <div className='flex cursor-pointer items-center gap-x-1'>
+        <img className='size-4' src='/assets/comment-dark.svg' alt='image' />
+        <p className='text-sm text-black/70'>{content.comments}</p>
+      </div>
+    </div>
     </div>
   );
 }
--- web/src/App/ui/Reactions.jsx
+++ web/src/App/ui/Reactions.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 
-function Reactions({ likes, comments }) {
+function Reactions({ likes, comments}) {
   return (
     <div className='flex items-center gap-x-5'>
       <div className='flex cursor-pointer items-center gap-x-1'>
-        <img className='size-4' src='/assets/heart-dark.svg' alt='image' />
+        <img className='size-4' src='/assets/eye.svg' alt='image' />
+        <p className='text-sm text-black/70'>1 Views</p>
+      </div>
+      <div className='flex cursor-pointer items-center gap-x-1'>
+        <img className='size-4' src='/assets/heart-light.svg' alt='image' />
         <p className='text-sm text-black/70'>{likes}</p>
       </div>
       <div className='flex cursor-pointer items-center gap-x-1'>
-        <img className='size-4' src='/assets/comment-dark.svg' alt='image' />
+        <img className='size-4' src='/assets/comment-light.svg' alt='image' />
         <p className='text-sm text-black/70'>{comments.length}</p>
       </div>
     </div>
