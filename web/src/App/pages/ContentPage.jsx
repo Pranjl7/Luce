660cd7b440d7907679427d3362bd7759dab374ee
Pranjl7 <pranjalsgbhatt@gmail.com>
2026-01-29 16:20:29 +0530

    feat: remove static data and add dynamic data from api

--- web/src/App/pages/ContentPage.jsx
+++ web/src/App/pages/ContentPage.jsx
@@ -1,49 +1,91 @@
-import React from 'react';
-import Reactions from '../ui/Reactions';
+import React, { useEffect, useRef, useState } from 'react';
+import { useParams } from 'react-router-dom';
 import ContentPageReactions from '../ui/Contentpage-Reactions';
 
 function ContentPage() {
+  const [user, setUser] = useState({});
+  const [content, setContent] = useState({});
+  const { useremailid, contenttitle } = useParams();
+
+  useEffect(() => {
+    fetchspecificcontent();
+  }, []);
+
+  async function fetchspecificcontent() {
+    try {
+      const response = await fetch(
+        `${import.meta.env.VITE_API_URL}/public/${useremailid}/${contenttitle}`
+      );
+      const data = await response.json();
+
+      if (!response.ok) {
+        alert(data.message);
+      } else {
+        setUser({ ...data.message });
+        setContent({
+          ...data.message.usercontents.find((e) => e.title == contenttitle),
+        });
+        console.log(content);
+      }
+    } catch (error) {
+      console.log(error);
+      alert('Error :' + error.message);
+    }
+  }
+
   return (
     <main className='scrollbar-simple mx-auto flex max-h-[90vh] max-w-172 flex-col gap-y-8 overflow-auto pt-4 pr-2'>
       <div className='flex flex-col gap-y-7'>
-        <div className='flex gap-x-2'>
-          <button className='rounded-full border border-black/20 px-3 py-1 text-sm font-medium text-black/70'>
-            Tech
-          </button>
-          <button className='rounded-full border border-black/20 px-3 py-1 text-sm font-medium text-black/70'>
-            Commerce
-          </button>
-        </div>
+        {content.tags && (
+          <div className='flex gap-x-3'>
+            {content.tags.map((tag, index) => (
+              <button
+                key={index}
+                className='rounded-full border border-black/20 px-3 py-1 text-sm font-medium text-black/70'
+              >
+                {tag}
+              </button>
+            ))}
+          </div>
+        )}
         <h1
           id='titlebox'
-          className='font-merriweather w-full text-4xl leading-12 font-bold tracking-tight'
+          className='font-merriweather w-full text-4xl leading-12 font-bold tracking-tight whitespace-pre-wrap'
         >
-          My Manager Quit Right After Q3 Planning. We All Thought She Was Crazy.
-          She Wasn't.
+          {content.title}
         </h1>
 
-        <ContentPageReactions />
+        <ContentPageReactions user={user} content={content} />
       </div>
 
-      <img
-        id='content-image'
-        className='w-full object-cover object-center'
-        src='https://miro.medium.com/v2/resize:fit:1100/format:webp/1*4V4IOqG27oqNgn1CTAZw-Q.png'
-        alt=''
-      />
+      {content.contentimage && (
+        <img
+          draggable='false'
+          loading='lazy'
+          id='content-image'
+          className='w-full object-cover object-center'
+          src={content.contentimage}
+          alt=''
+        />
+      )}
 
       <div
         id='contentbox'
-        className='font-merriweather w-full text-lg leading-8 tracking-wide text-black/80'
+        className='font-merriweather w-full text-lg leading-8 tracking-wide whitespace-pre-wrap text-black/80'
       >
-        We chose some of the most currently popular niches and selected several
-        blog examples for each niche. We researched every blog to learn more
-        about CMS'es and the themes that they're using. <br /> <br />
-        Finally, we included the sources of income for every blog, which can
-        help you to understand how blogs make money. <br /> <br /> This list
-        should inspire you to create your own personal blog in a niche that you
-        are passionate about. This is an opportunity to learn from the best in
-        the online business.
+        {content.content}
+      </div>
+
+      <div className='mt-5 flex items-center justify-center gap-x-1 py-2 text-sm tracking-wide'>
+        <p>Build and designed by</p>
+        <a className='underline' href='https://github.com/Pranjl7'>
+          Pranjal
+        </a>
+        <img
+          className='inline-block size-3.5'
+          src='/assets/heart-dark.svg'
+          alt=''
+        />
       </div>
     </main>
   );
