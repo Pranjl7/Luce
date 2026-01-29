cb4c78b2da524e38e4019452c5b026c38083dbab
Pranjl7 <pranjalsgbhatt@gmail.com>
2026-01-29 16:13:54 +0530

    feat: update profile route to stats

--- api/src/routes/userRoutes.js
+++ api/src/routes/userRoutes.js
@@ -2,7 +2,7 @@ import express from 'express';
 const router = express.Router();
 import { ClerkExpressRequireAuth } from '@clerk/clerk-sdk-node';
 
-import { usersync, userbookmarks, userprofile, userfollowing } from '../controllers/userController.js';
+import { usersync, userbookmarks, userstats, userfollowing } from '../controllers/userController.js';
 
 // router.get('/:emailid', requireAuth(), user);
 
@@ -10,7 +10,7 @@ router.post('/sync', ClerkExpressRequireAuth(),  usersync);
 
 router.get('/library', ClerkExpressRequireAuth(), userbookmarks);
 
-router.get('/profile', ClerkExpressRequireAuth(), userprofile);
+router.get('/stats', ClerkExpressRequireAuth(), userstats);
 
 router.get('/following', ClerkExpressRequireAuth(), userfollowing);
 
