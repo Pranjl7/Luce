9f3eabefde40e56a2e54b825ba7b3a60660a5ba7
Pranjl7 <pranjalsgbhatt@gmail.com>
2026-01-29 16:11:11 +0530

    feat: add logic for fetching content specific webpage

--- api/src/controllers/publicController.js
+++ api/src/controllers/publicController.js
@@ -1,4 +1,5 @@
 import { Content } from '../models/contentModel.js';
+import { User } from '../models/userModel.js';
 
 export async function fetchcontent(req, res) {
   try {
@@ -16,11 +17,34 @@ export async function fetchcontent(req, res) {
   }
 }
 
-export async function fetchcontentpage(req, res){
+export async function fetchcontentpage(req, res) {
   try {
-    
+    const useremailid = req.params.useremailid;
+    const contenttitle = req.params.contenttitle;
+
+    if (!useremailid || !contenttitle) {
+      res.status(404).json({
+        success: false,
+        message: 'No Page Credentials Received',
+      });
+      return;
+    }
+
+    const user = await User.findOne({ emailid: useremailid }).populate('usercontents');
+    if (!user) {
+      res.status(404).json({
+        success: false,
+        message: 'User not found',
+      });
+      return;
+    }
+
+    res.status(200).json({
+      success: true,
+      message: user,
+    });
   } catch (error) {
-    console.log(error)
+    console.log(error);
     res.status(500).json({
       success: false,
       message: 'Something went wrong, plz try again',
