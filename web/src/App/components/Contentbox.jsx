79a53d3e1b36c5563f25581069ae656b417d3fcf
Pranjl7 <pranjalsgbhatt@gmail.com>
2026-01-29 16:16:31 +0530

    feat: wrap component inside link tag for navigation and add verified svg

--- web/src/App/components/Contentbox.jsx
+++ web/src/App/components/Contentbox.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import HomePageReactions from '../ui/Homepage-Reactions';
+import Reactions from '../ui/Reactions';
 
 function Contentbox({
   thumbnail,
@@ -20,7 +20,7 @@ function Contentbox({
 
   return (
     <Link
-      to={'/contentpage'}
+      to={`/${author.emailid}/${title}`}
       className='flex w-full items-center justify-between overflow-hidden border-b border-black/10 pb-4'
     >
       {/* Content-side */}
@@ -31,7 +31,14 @@ function Contentbox({
             src={author.avatarurl}
             alt='profile-logo'
           />
-          <p className='text-sm'>{author.username}</p>
+          <p className='text-center text-sm'>
+            {author.username}
+            <img
+              src='/assets/verified.svg'
+              alt='image'
+              className='ml-1 inline-block size-4'
+            />
+          </p>
         </div>
         <div className='flex w-full flex-col gap-y-2'>
           <h1 className='font-merriweather line-clamp-3 max-h-24 w-full overflow-hidden text-2xl font-bold tracking-tight'>
@@ -42,17 +49,24 @@ function Contentbox({
           </p>
         </div>
 
-        <HomePageReactions
-          likes={likes}
-          comments={comments}
-          createdat={createdat}
-        />
+        <div className='flex items-center justify-between'>
+          <Reactions likes={likes} comments={comments} />
+          <div className='flex items-center gap-x-4'>
+            <p className='text-sm text-black/70'>{createdat}</p>
+            <img
+              className='size-6'
+              src='/assets/bookmark-light.svg'
+              alt='image'
+            />
+          </div>
+        </div>
       </div>
 
       {/* Image-side */}
       {image && (
         <div className='w-[22%]'>
           <img
+            draggable='false'
             loading='lazy'
             className='aspect-16/10 w-full object-cover object-center'
             src={thumbnail != null ? thumbnail : contentimage}
