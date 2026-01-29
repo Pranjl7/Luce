2a672c005588833d6d8a7c57595fdcafb7cc0f2f
Pranjl7 <pranjalsgbhatt@gmail.com>
2026-01-29 16:22:27 +0530

    revert: remove profile route

--- web/src/App/routes/AppRoutes.jsx
+++ web/src/App/routes/AppRoutes.jsx
@@ -2,17 +2,19 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from '../pages/Home';
 import Library from '../pages/Library';
-import Profile from '../pages/Profile';
 import Write from '../pages/Write';
 import ContentPage from '../pages/ContentPage';
+import Stats from '../pages/Stats';
+import Trending from '../pages/Trending';
 
 function AppRoutes() {
   return (
     <Routes>
       <Route path='/' element={<Home />} />
-      <Route path='/contentpage' element={<ContentPage />} />
+      <Route path='/:useremailid/:contenttitle' element={<ContentPage />} />
       <Route path='/me/library' element={<Library />} />
-      <Route path='/me/profile' element={<Profile />} />
+      <Route path='/me/trending' element={<Trending />} />
+      <Route path='/me/stats' element={<Stats />} />
       <Route path='/me/write' element={<Write />} />
     </Routes>
   );
